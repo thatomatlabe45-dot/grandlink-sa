73dedc5771c96902c6389036c097ea81aa613c28
thatomatlabe45-dot <thatomatlabe45@gmail.com>
2026-07-22 12:45:35 +0200

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
