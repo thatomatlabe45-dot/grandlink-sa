1bbc90d7a29374e0a2bea24ed3edfe9b33bbf104
thatomatlabe45-dot <thatomatlabe45@gmail.com>
2026-08-12 09:57:31 +0200

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -3,8 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabase";
-
+import { supabase } from "../../lib/supabase";
 export default function LoginPage() {
   const router = useRouter();
 
