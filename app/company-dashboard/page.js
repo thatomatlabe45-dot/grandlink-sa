106c49d2fecdc216d49d9db9069fc48682e90eb9
thatomatlabe45-dot <thatomatlabe45@gmail.com>
2026-09-01 13:17:15 +0200

    Update page.js

--- app/company-dashboard/page.js
+++ app/company-dashboard/page.js
@@ -1071,7 +1071,7 @@ export default function CompanyDashboard() {
               <button
                 onClick={() =>
                   router.push(
-                    "/internship"
+                    "/internships"
                   )
                 }
                 style={
